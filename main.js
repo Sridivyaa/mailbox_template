12b709f05af4fa0eaf45300ad89b76e103b91a46
Sridivyaa <51296221+Sridivyaa@users.noreply.github.com>
2022-04-08 20:34:14 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function inbox(){
+    document.getElementById("inbox").style.backgroundColor="white";
+    document.getElementById("inbox").style.color="black";
+    document.getElementById("inboxlabel").style.backgroundColor="rgb(242, 83, 51)";
+    document.getElementById("inboxlabel").style.color="white";
+
+}
